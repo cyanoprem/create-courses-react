faaac43eae33c849a2041f4424d945f2928c17ff
Cyano Prem <66469412+cyanoprem@users.noreply.github.com>
2024-05-25 22:06:22 +0530

    Add Post Request

--- src/components/CreateCourse.jsx
+++ src/components/CreateCourse.jsx
@@ -1,9 +1,12 @@
-import { useState } from "react"
+import { useState, useContext } from "react"
+import { coursesContext } from "../providers/CoursesContextProvider"
 
 const CreateCourse = () => {
 
   const [courseName, setCourseName] = useState("")
   const [courseDescription, setCourseDescription] = useState("")
+
+  const {createCourse} = useContext(coursesContext)
 
   const captureCourseName = (event) => {
     setCourseName(event.target.value)
@@ -13,9 +16,16 @@ const CreateCourse = () => {
     setCourseDescription(event.target.value)
   }
 
+  const createCourseFn = () => {
+    createCourse(courseName, courseDescription)
+    setCourseName("")
+    setCourseDescription("")
+  }
+
   return <>
-    <input type="text" placeholder="Enter Course Name" onChange={captureCourseName}/>
-    <input type="text" placeholder="Enter Course Desc" onChange={captureCourseDescription}/>
+    <input type="text" placeholder="Enter Course Name" onChange={captureCourseName} value={courseName}/>
+    <input type="text" placeholder="Enter Course Desc" onChange={captureCourseDescription} value={courseDescription}/>
+    <button onClick={createCourseFn}>Create Course</button>
   </>
 }
 
